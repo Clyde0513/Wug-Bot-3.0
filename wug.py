c6f623fd939d7b9831a961950a87670fe1235df2
Clyde0513 <iclyde.villacrusis@gmail.com>
2024-09-20 15:18:20 -0700

    Added multiple try/except exceptions to 3 of the functions

--- wug.py
+++ wug.py
@@ -89,66 +89,76 @@ class MyDiscord(discord.Client):
 
     async def on_message(self, message, *args, **kwargs):
         allowed_channels = [int(channel.strip()) for channel in ALLOWED_CHANNELS]
-        if message.author.bot or message.author == self.user:
-            return
+        try:
+            if message.author.bot or message.author == self.user:
+                return
 
-        if message.channel.id in allowed_channels:
-            commands_dict = {
-                '$wug' : self.handle_wug,
-                '$ipa ' : self.handle_ipa,
-                '$translate ' : self.handle_translation,
-                '$help' : self.handle_help,
-                '$syllabify ' : self.handle_syllabification,
-                '$tree ' : self.handle_syntax_tree
-            }
-            for command, handler in commands_dict.items(): # Command is key; handler is value
-                if message.content.startswith(command):
-                    user_id = message.author.id
-                    if user_id in cooldown:
-                        await message.reply("Please stop spamming! Wait 5 seconds.", mention_author=False)
-                        return
-                    
-                    cooldown[user_id] = time.time()
-                    await handler(message)
-                    await message.add_reaction("👍")
-                    await asyncio.sleep(5)
-                    del cooldown[user_id]
-                    break
+            if message.channel.id in allowed_channels:
+                commands_dict = {
+                    '$wug' : self.handle_wug,
+                    '$ipa ' : self.handle_ipa,
+                    '$translate ' : self.handle_translation,
+                    '$help' : self.handle_help,
+                    '$syllabify ' : self.handle_syllabification,
+                    '$tree ' : self.handle_syntax_tree
+                }
+                for command, handler in commands_dict.items(): # Command is key; handler is value
+                    if message.content.startswith(command):
+                        user_id = message.author.id
+                        if user_id in cooldown:
+                            await message.reply("Please stop spamming! Wait 5 seconds.", mention_author=False)
+                            return
+                        
+                        cooldown[user_id] = time.time()
+                        await handler(message)
+                        await message.add_reaction("👍")
+                        await asyncio.sleep(5)
+                        del cooldown[user_id]
+                        break
+        except Exception as e:
+            await message.channel.send(f'Sorry! An error occurred: {e}')
                 
     async def handle_wug(self,message):
         await message.channel.send("Wug's here!")
         
     async def handle_ipa(self,message):
-        text_to_translate = message.content[len('$ipa '):].strip()
-        for sent in sentences(text_to_translate,lang="en-us"):
-            for word in sent:
-                if word.phonemes:
-                    phonemes_str = ' '.join(word.phonemes)
-                    await message.channel.send(f'IPA Translation: /{phonemes_str}/')
+        try:
+            text_to_translate = message.content[len('$ipa '):].strip()
+            for sent in sentences(text_to_translate,lang="en-us"):
+                for word in sent:
+                    if word.phonemes:
+                        phonemes_str = ' '.join(word.phonemes)
+                        await message.channel.send(f'IPA Translation: /{phonemes_str}/')
+        except Exception as e:
+            await message.channel.send(f'Sorry! An error occurred: {e}')
 
     async def handle_translation(self,message):
-        params = message.content[len('$translate '):].strip().split(maxsplit=2)
-        if len(params) != 3:
-            await message.reply("Please send messages in this format: $translate [from-code] [to-code] [word or sentence].", mention_author=False)
-            return
-        from_code, to_code, text_to_translate = params
-        if not (from_code, to_code) in mappings:
-            if not from_code in codes or not to_code in codes:
-                await message.reply('Please enter an available language (type $help for a list of available languages).', mention_author=False)
+        try:
+            params = message.content[len('$translate '):].strip().split(maxsplit=2)
+            if len(params) != 3:
+                await message.reply("Please send messages in this format: $translate [from-code] [to-code] [word or sentence].", mention_author=False)
                 return
-            for code in codes:
-                if code == from_code or code == to_code:
-                    continue
-                if (from_code, code) in mappings and (code, to_code) in mappings:
-                    tempTranslation = argostranslate.translate.translate(text_to_translate,from_code,code)
-                    translatedText = argostranslate.translate.translate(tempTranslation,code,to_code)
-                    await message.channel.send(f'Translation: {translatedText}')
+            from_code, to_code, text_to_translate = params
+            if not (from_code, to_code) in mappings:
+                if not from_code in codes or not to_code in codes:
+                    await message.reply('Please enter an available language (type $help for a list of available languages).', mention_author=False)
                     return
-            await message.reply('Sorry, translations between these languages are not yet supported.', mention_author=False)
-            return
-        else:
-            translatedText = argostranslate.translate.translate(text_to_translate,from_code,to_code)
-            await message.channel.send(f'Translation: {translatedText}')
+                for code in codes:
+                    if code == from_code or code == to_code:
+                        continue
+                    if (from_code, code) in mappings and (code, to_code) in mappings:
+                        tempTranslation = argostranslate.translate.translate(text_to_translate,from_code,code)
+                        translatedText = argostranslate.translate.translate(tempTranslation,code,to_code)
+                        await message.channel.send(f'Translation: {translatedText}')
+                        return
+                await message.reply('Sorry, translations between these languages are not yet supported.', mention_author=False)
+                return
+            else:
+                translatedText = argostranslate.translate.translate(text_to_translate,from_code,to_code)
+                await message.channel.send(f'Translatiosn: {translatedText}')
+        except Exception as e:
+            await message.channel.send(f'Sorry! An error occurred: {e}')
+
 
     async def handle_syllabification(self,message):        
         # Source: https://en.wikipedia.org/wiki/IPA_vowel_chart_with_audio
@@ -165,117 +175,122 @@ class MyDiscord(discord.Client):
         'mj','fj','vj','θj','sj','zj','hj','lj','sp','st','sk','sm','sn','sf','sθ','spl','skl','spɹ','stɹ','skw',
         'spj','stj','skj','smj','snj','sfɹ'])
 
-        def remove_diacritics(s):
-            # Normalize to NFD (Normalization Form D) to decompose characters
-            s_decomposed = unicodedata.normalize('NFD', s)
-            
-            # Filter out combining diacritic marks
-            s_no_diacritics = ''.join(c for c in s_decomposed if not unicodedata.combining(c))
-        
-            # Optionally, normalize back to NFC (Normalization Form C) if needed
-            return unicodedata.normalize('NFC', s_no_diacritics)
-
-        def find_onsets(cluster):
-            n = len(cluster)
-            lengths = []
-    
-            # Iterate over all possible starting points for substrings
-            for start in range(len(cluster)):
-                # Iterate over all possible ending points for substrings starting from `start`
-                for end in range(start + 1, n + 1):
-                    substring = cluster[start:end]
-                    if substring in onsetClusters:
-                        lengths.append(((end-start), start, end))
-        
-            return sorted(lengths, reverse=True, key=lambda x: x[0])
-        
-        cleaned_string = re.sub(r'[^a-zA-Z\s-]', '', message.content[len('$syllabify '):])
-        reply = ''
-        words = []
-        ipa = []
-
-        # Convert each word in the sentence to IPA
-        for sent in sentences(cleaned_string,lang="en-us"):
-            for word in sent:
-                if (word.phonemes):
-                    words.append(word.text)
-                    ipa.append(remove_diacritics(((''.join(word.phonemes)).replace("ˈ","")).replace("ˌ","")))
-        
-        if not ipa:
-            await message.reply('Please include at least one alphabetic character in your prompt!', mention_author=False)
-            return
-
-        for word in ipa:
-            reply += '•••••••••••••••\n'
-                    
-            reply += f'Word: {words[ipa.index(word)]} ({word})\n'
-            syllables = []
-            i = 0
-
-            while i < len(word):
-                # Find the next vowel or diphthong
-                j = i
-                while j < len(word) and word[j] not in vowels:
-                    j += 1
-                if j == len(word):
-                    break
-
-                # Check for diphthong
-                if j < len(word) - 1 and word[j:j+2] in diphthongs:
-                    nucleus_end = j + 1
-                else:
-                    nucleus_end = j
-
-                # Find the onset of the next syllable
-                k = nucleus_end + 1
-                while k < len(word) and word[k] not in vowels:
-                    k += 1
+        try:
+            def remove_diacritics(s):
+                # Normalize to NFD (Normalization Form D) to decompose characters
+                s_decomposed = unicodedata.normalize('NFD', s)
                 
-                if k < len(word):
-                    onsets = find_onsets(word[nucleus_end+1:k])
-                    
-                    # Find the longest onset cluster whose next character is a vowel
-                    if onsets:
-                        for onset in onsets:
-                             if (word[nucleus_end+1+onset[0]+onset[1]] in vowels):
-                                coda_end = nucleus_end+onset[1]
-                                break
-                    else:
-                        coda_end = k-1
-                else:
-                    coda_end = len(word) - 1
-
-                syllables.append((i, coda_end))
-                i = coda_end + 1
+                # Filter out combining diacritic marks
+                s_no_diacritics = ''.join(c for c in s_decomposed if not unicodedata.combining(c))
             
-            reply += f'Syllable count: {len(syllables)}\n'
+                # Optionally, normalize back to NFC (Normalization Form C) if needed
+                return unicodedata.normalize('NFC', s_no_diacritics)
 
-            for idx, (start, end) in enumerate(syllables):
-                syllable_text = word[start:end+1]
-                reply += f'  Syllable: {syllable_text}\n'
+            def find_onsets(cluster):
+                n = len(cluster)
+                lengths = []
+        
+                # Iterate over all possible starting points for substrings
+                for start in range(len(cluster)):
+                    # Iterate over all possible ending points for substrings starting from `start`
+                    for end in range(start + 1, n + 1):
+                        substring = cluster[start:end]
+                        if substring in onsetClusters:
+                            lengths.append(((end-start), start, end))
+            
+                return sorted(lengths, reverse=True, key=lambda x: x[0])
+            
+            cleaned_string = re.sub(r'[^a-zA-Z\s-]', '', message.content[len('$syllabify '):])
+            reply = ''
+            words = []
+            ipa = []
 
-                # Find onset
-                j = start
-                while j <= end and word[j] not in vowels:
-                    j += 1
-                if j > start:
-                    reply += f'     Onset: {word[start:j]}\n'
-                else:
-                    reply += f'     Onset: none\n'
+            # Convert each word in the sentence to IPA
+            for sent in sentences(cleaned_string,lang="en-us"):
+                for word in sent:
+                    if (word.phonemes):
+                        words.append(word.text)
+                        ipa.append(remove_diacritics(((''.join(word.phonemes)).replace("ˈ","")).replace("ˌ","")))
+            
+            if not ipa:
+                await message.reply('Please include at least one alphabetic character in your prompt!', mention_author=False)
+                return
 
-                # Find nucleus
-                k = j
-                while k <= end and (word[k] in vowels or (k < end and word[k:k+2] in diphthongs)):
-                    k += 1
-                reply += f'     Nucleus: {word[j:k]}\n'
+            for word in ipa:
+                reply += '•••••••••••••••\n'
+                        
+                reply += f'Word: {words[ipa.index(word)]} ({word})\n'
+                syllables = []
+                i = 0
 
-                # Find coda
-                if k <= end:
-                    reply += f'     Coda: {word[k:end+1]}\n'
-                else:
-                    reply += f'     Coda: none\n'
+                while i < len(word):
+                    # Find the next vowel or diphthong
+                    j = i
+                    while j < len(word) and word[j] not in vowels:
+                        j += 1
+                    if j == len(word):
+                        break
 
-        await message.channel.send(reply)
+                    # Check for diphthong
+                    if j < len(word) - 1 and word[j:j+2] in diphthongs:
+                        nucleus_end = j + 1
+                    else:
+                        nucleus_end = j
+
+                    # Find the onset of the next syllable
+                    k = nucleus_end + 1
+                    while k < len(word) and word[k] not in vowels:
+                        k += 1
+                    
+                    if k < len(word):
+                        onsets = find_onsets(word[nucleus_end+1:k])
+                        
+                        # Find the longest onset cluster whose next character is a vowel
+                        if onsets:
+                            for onset in onsets:
+                                if (word[nucleus_end+1+onset[0]+onset[1]] in vowels):
+                                    coda_end = nucleus_end+onset[1]
+                                    break
+                        else:
+                            coda_end = k-1
+                    else:
+                        coda_end = len(word) - 1
+
+                    syllables.append((i, coda_end))
+                    i = coda_end + 1
+                
+                reply += f'Syllable count: {len(syllables)}\n'
+
+                for idx, (start, end) in enumerate(syllables):
+                    syllable_text = word[start:end+1]
+                    reply += f'  Syllable: {syllable_text}\n'
+
+                    # Find onset
+                    j = start
+                    while j <= end and word[j] not in vowels:
+                        j += 1
+                    if j > start:
+                        reply += f'     Onset: {word[start:j]}\n'
+                    else:
+                        reply += f'     Onset: none\n'
+
+                    # Find nucleus
+                    k = j
+                    while k <= end and (word[k] in vowels or (k < end and word[k:k+2] in diphthongs)):
+                        k += 1
+                    reply += f'     Nucleus: {word[j:k]}\n'
+
+                    # Find coda
+                    if k <= end:
+                        reply += f'     Coda: {word[k:end+1]}\n'
+                    else:
+                        reply += f'     Coda: none\n'
+
+            await message.channel.send(reply)
+            
+        except Exception as e:
+            await message.channel.send(f'Sorry! An error occurred: {e}')
+        
     
     async def handle_syntax_tree(self, message):
         # You only have to download these once
